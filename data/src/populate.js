2cdaa8285b3b5af34de0683bee8ba9a9710cf2a6
Steve <stevenwasi@gmail.com>
2015-02-17 20:04:56 -0500

    Fixed bug in populate.js

--- data/src/populate.js
+++ data/src/populate.js
@@ -7,31 +7,29 @@ var mongoose    = require('mongoose'),
     data        = require('../partType.json');
 
 /*Check if PartType is already populated */
-var partType_exists = PartType.count({}, function(err, count){
-    if (err) console.log(err);
-    return count;
+PartType.count({}, function(err, count){
+    if (err) return console.log(err);
+
+    if(!count){
+        var err, el;
+
+        function errorHandler(err, el){
+            if (err) console.log(err);
+            //else console.log(el + ' added to database');
+        }
+
+        /* For each partType */
+        for(var i = 0; i < data.length; i++ ){
+            /* Save partType */
+            var partType = new PartType(data[i]);
+            partType.save(errorHandler(err, partType));
+            /* Save partType to inventory */
+            var inventory = new Inventory({ Type: partType._id, quantity: 0});
+            inventory.save(errorHandler(err, inventory));
+        }
+        console.log('MongoDB has been populated...');
+    } else {
+        console.log('MongoDB is already populated...');
+    }
 });
-
-
-if(!partType_exists){
-    var err, el;
-
-    function errorHandler(err, el){
-        if (err) console.log(err);
-        //else console.log(el + ' added to database');
-    }
-
-    /* For each partType */
-    for(var i = 0; i < data.length; i++ ){
-        /* Save partType */
-        var partType = new PartType(data[i]);
-        partType.save(errorHandler(err, partType));
-        /* Save partType to inventory */
-        var inventory = new Inventory({ Type: partType._id, quantity: 0});
-        inventory.save(errorHandler(err, inventory));
-    }
-    console.log('MongoDB has been populated...');
-} else {
-    console.log('MongoDB is already populated...');
-}
 /////////////////////////////////////////////////////////////
