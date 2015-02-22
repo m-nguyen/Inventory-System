02797c7f432e642c86ebb2433471b9356a53a97f
Steve <stevenwasi@gmail.com>
2015-02-22 09:51:03 -0500

    Forgot to delete the routes for add and remove

--- public/modules/inventory/config/inventory.client.routes.js
+++ public/modules/inventory/config/inventory.client.routes.js
@@ -6,14 +6,6 @@ angular.module('inventory').config(['$stateProvider', '$urlRouterProvider',
 	function($stateProvider) {
 		// inventory state routing
 		$stateProvider.
-		state('addPart', {
-			url: '/inventory/add',
-			templateUrl: 'modules/inventory/views/add.client.view.html'
-		}).
-		state('removePart', {
-			url: '/inventory/:partId/remove',
-			templateUrl: 'modules/inventory/views/remove.client.view.html'
-		}).
 		state('createPart', {
 			url: '/inventory/create',
 			templateUrl: 'modules/inventory/views/create.client.view.html'
