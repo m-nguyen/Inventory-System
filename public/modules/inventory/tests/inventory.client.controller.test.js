91d34c5d7ac7c3e08c9fc9d35548dd0de6910fd5
Steve <stevenwasi@gmail.com>
2015-02-23 11:45:25 -0500

    fixed problem with create view radio button values

--- public/modules/inventory/tests/inventory.client.controller.test.js
+++ public/modules/inventory/tests/inventory.client.controller.test.js
@@ -53,8 +53,16 @@
 		it('$scope.find() should create an array with at least one partType object fetched from XHR', inject(function(Inventory) {
 			// Create sample partType using the Inventory service
 			var samplePart = new Inventory({
-				title: 'An Part about MEAN',
-				content: 'MEAN rocks!'
+                category: 'Sample Category',
+                partName: 'Sample Name',
+                vendor: 'Sample Vendor',
+                vndrPartNmbr: '123',
+                manufacturer: 'Sample Manufacturer',
+                mnfPartNmbr: '456',
+                price: '12',
+                GX5_amount: '1',
+                GX35_amount: '2',
+                quantity:'3'
 			});
 
 			// Create a sample inventory array that includes the new partType
@@ -74,8 +82,16 @@
 		it('$scope.findOne() should create an array with one partType object fetched from XHR using a partId URL parameter', inject(function(Inventory) {
 			// Define a sample partType object
 			var samplePart = new Inventory({
-				title: 'An Part about MEAN',
-				content: 'MEAN rocks!'
+                category: 'Sample Category',
+                partName: 'Sample Name',
+                vendor: 'Sample Vendor',
+                vndrPartNmbr: '123',
+                manufacturer: 'Sample Manufacturer',
+                mnfPartNmbr: '456',
+                price: '12',
+                GX5_amount: '1',
+                GX35_amount: '2',
+                quantity:'3'
 			});
 
 			// Set the URL parameter
@@ -95,20 +111,44 @@
 		it('$scope.create() with valid form data should send a POST request with the form input values and then locate to new object URL', inject(function(Inventory) {
 			// Create a sample partType object
 			var samplePartPostData = new Inventory({
-				title: 'An Part about MEAN',
-				content: 'MEAN rocks!'
+                category: 'Sample Category',
+                partName: 'Sample Name',
+                vendor: 'Sample Vendor',
+                vndrPartNmbr: '123',
+                manufacturer: 'Sample Manufacturer',
+                mnfPartNmbr: '456',
+                price: '12',
+                GX5_amount: '1',
+                GX35_amount: '2',
+                quantity:'3'
 			});
 
 			// Create a sample partType response
 			var samplePartResponse = new Inventory({
 				_id: '525cf20451979dea2c000001',
-				title: 'An Part about MEAN',
-				content: 'MEAN rocks!'
+                category: 'Sample Category',
+                partName: 'Sample Name',
+                vendor: 'Sample Vendor',
+                vndrPartNmbr: '123',
+                manufacturer: 'Sample Manufacturer',
+                mnfPartNmbr: '456',
+                price: '12',
+                GX5_amount: '1',
+                GX35_amount: '2',
+                quantity:'3'
 			});
 
 			// Fixture mock form input values
-			scope.title = 'An Part about MEAN';
-			scope.content = 'MEAN rocks!';
+            scope.category = 'Sample Category';
+            scope.partName = 'Sample Name';
+            scope.vendor = 'Sample Vendor';
+            scope.vndrPartNmbr = '123';
+            scope.manufacturer = 'Sample Manufacturer';
+            scope.mnfPartNmbr = '456';
+            scope.price = '12';
+            scope.GX5_amount = '1';
+            scope.GX35_amount = '2';
+            scope.quantity = '3';
 
 			// Set POST response
 			$httpBackend.expectPOST('inventory', samplePartPostData).respond(samplePartResponse);
@@ -118,8 +158,16 @@
 			$httpBackend.flush();
 
 			// Test form inputs are reset
-			expect(scope.title).toEqual('');
-			expect(scope.content).toEqual('');
+			expect(scope.category).toEqual('');
+			expect(scope.partName).toEqual('');
+            expect(scope.vendor).toEqual('');
+            expect(scope.vndrPartNmbr).toEqual('');
+            expect(scope.manufacturer).toEqual('');
+            expect(scope.mnfPartNmbr).toEqual('');
+            expect(scope.price).toEqual('');
+            expect(scope.GX5_amount).toEqual('');
+            expect(scope.GX35_amount).toEqual('');
+            expect(scope.quantity).toEqual('');
 
 			// Test URL redirection after the partType was created
 			expect($location.path()).toBe('/inventory/' + samplePartResponse._id);
@@ -129,8 +177,16 @@
 			// Define a sample partType put data
 			var samplePartPutData = new Inventory({
 				_id: '525cf20451979dea2c000001',
-				title: 'An Part about MEAN',
-				content: 'MEAN Rocks!'
+                category: 'Sample Category',
+                partName: 'Sample Name',
+                vendor: 'Sample Vendor',
+                vndrPartNmbr: '123',
+                manufacturer: 'Sample Manufacturer',
+                mnfPartNmbr: '456',
+                price: '12',
+                GX5_amount: '1',
+                GX35_amount: '2',
+                quantity:'3'
 			});
 
 			// Mock partType in scope
